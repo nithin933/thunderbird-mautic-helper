da3ecb4241f37cec799e730ee44aef330ab16195
nithin933 <nithin933@gmail.com>
2021-07-15 09:22:56 +0400

    Improved recipient identification

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 browser.composeAction.onClicked.addListener(async (tab) => {
   let details = await browser.compose.getComposeDetails(tab.id);
-  console.log(details);
+  // console.log(details);
 
   if (details.isPlainText) {
     let body = details.plainTextBody;
 
-    console.log("this be plain text, go html");
+    // console.log("this be plain text, go html");
   } else {
     let document = new DOMParser().parseFromString(details.body, "text/html");
 
@@ -23,30 +23,39 @@ browser.composeAction.onClicked.addListener(async (tab) => {
     } else {
       mautic_secret = "examplesecret";
       mautic_url = "https://mautic.example.com"; // like https://mautic.example.com
+            fullemail = details.to[0];
+      var regex = /<(.*)>/g; // The actual regex
+      var matches = regex.exec(fullemail);
+      console.log("matches: ", matches);
+      if (matches) {
+        correctedmail = matches[1];
+      } else {
+        correctedmail = details.to[0];
+      }
 
       var qs =
         "from=" +
         encodeURIComponent("SENDEREMAILID") +
         "&email=" +
-        encodeURIComponent(details.to[0]) +
+        encodeURIComponent(correctedmail)+
         "&subject=" +
         encodeURIComponent(details.subject) +
         "&body=" +
         encodeURIComponent(details.body);
-      console.log("to: ", details.to[0]);
-      console.log("subject: ", details.subject);
+      // console.log("to: ", details.to[0]);
+      // console.log("subject: ", details.subject);
 
-      console.log("qs: ", qs);
+      // console.log("qs: ", qs);
       var d = encodeURIComponent(btoa(pako.gzip(qs, { to: "string" })));
-      console.log("d: ", d);
+      // console.log("d: ", d);
 
       var cr = CryptoJS.PHP_CRYPT_MD5(d, "$1$" + mautic_secret);
 
-      console.log("cr: ", cr);
+      // console.log("cr: ", cr);
 
       var hash = crc32(cr).toString();
       while (hash.length < 8) hash = "0" + hash.toString();
-      console.log("hash: ", hash);
+      // console.log("hash: ", hash);
 
       var trackingGif =
         '<img style="display: none;" height="1" width="1" src="' +
@@ -57,7 +66,7 @@ browser.composeAction.onClicked.addListener(async (tab) => {
         hash +
         '" ' +
         'alt="ThunderMautic">';
-      console.log(trackingGif);
+      // console.log(trackingGif);
 
       if (document.getElementById("mtech")) {
         document.getElementById("mtech").remove();
@@ -71,6 +80,7 @@ browser.composeAction.onClicked.addListener(async (tab) => {
       let html = new XMLSerializer().serializeToString(document);
       console.log(html);
       browser.compose.setComposeDetails(tab.id, { body: html });
+      console.log("done");
     }
   }
 });
